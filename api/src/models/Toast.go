072bc5de4f37235394fa6ff9cac7437d29e6b098
MintzyG <ericbraga2000@gmail.com>
2024-10-27 15:44:05 -0300

    Created the toast model

---
+++ api/src/models/Toast.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Toast struct {
+	SessionID   int           `json:"session_id"`
+	UserID      uuid.UUID     `json:"user_id"`
+	SessionName string        `json:"session_name"`
+	Description string        `json:"description"`
+	StartTime   time.Time     `json:"start_time"`
+	EndTime     time.Time     `json:"end_time"`
+	Duration    time.Duration `json:"duration"`
+	CategoryID  *int          `json:"category_id"`
+}
