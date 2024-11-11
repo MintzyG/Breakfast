68e16955f946f829a35f342518e40fc44d97ca8b
MintzyG <ericbraga2000@gmail.com>
2024-11-10 22:26:02 -0300

    Created Maple Model

---
+++ api/src/models/Maple.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Maple struct {
+  HabitID int `json:"habit_id"`
+  UserID  uuid.UUID `json:"user_id"`
+  Title string `json:"title"`
+  SmallestUnit string `json:"smallest_unit"`
+  CurrStreak int `json:"curr_streak"`
+  HighestStreak int `json:"highest_streak"`
+  DaysPerformed []time.Time `json:"days_performed"`
+}
