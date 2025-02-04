54265ce7007733355becb8546e42fce13de4aa39
MintzyG <ericbraga2000@gmail.com>
2025-02-04 00:12:21 -0300

    Created a model validator

---
+++ api/src2/internal/models/validate.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+  "github.com/go-playground/validator/v10"
+)
+
+var validate = validator.New()
+
+func ValidateModel(model interface{}) (map[string]string, error) {
+	if err := validate.Struct(model); err != nil {
+		validationErrors := make(map[string]string)
+		for _, err := range err.(validator.ValidationErrors) {
+			validationErrors[err.Field()] = err.Tag()
+		}
+		return validationErrors, err
+	}
+	return nil, nil
+}
