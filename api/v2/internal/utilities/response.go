50ee05a80e0ed54ff50476e8cefbebc286e866c6
MintzyG <ericbraga2000@gmail.com>
2025-02-11 13:58:05 -0300

    Updated Send() utiility

--- api/v2/internal/utilities/response.go
+++ api/v2/internal/utilities/response.go
@@ -7,18 +7,23 @@ import (
 
 type Response struct {
 	Msg  string      `json:"message,omitempty"`
-	Data interface{} `json:"data,omitempty"`
+	Payload interface{} `json:"data,omitempty"`
 }
 
 func (r *Response) Send(w http.ResponseWriter, code int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(r)
+
+	if r.Msg == "" {
+		json.NewEncoder(w).Encode(r.Payload)
+	} else {
+		json.NewEncoder(w).Encode(Response{Msg: r.Msg, Payload: r.Payload})
+	}
 }
 
 func Send(w http.ResponseWriter, msg string, data interface{}, code int) {
 	var r Response
 	r.Msg = msg
-	r.Data = data
+	r.Payload = data
 	r.Send(w, code)
 }
