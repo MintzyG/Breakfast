10fb3ac051bc0d794bdd0c8a31e84c7ee112895c
GitHub Action <action@github.com>
2025-02-13 15:18:26 +0000

    Auto-format Go code

--- api/v2/internal/models/pancake.go
+++ api/v2/internal/models/pancake.go
@@ -12,7 +12,7 @@ type Pancake struct {
 	Emoji   string    `gorm:"type:varchar(31)" json:"emoji"`
 	Title   string    `gorm:"type:varchar(255);not null" json:"title" validate:"required"`
 	Content string    `gorm:"type:text;not null" json:"content" validate:"required"`
-  Color   string    `gorm:"type:varchar(6);default:4CAF50" json:"color"`
+	Color   string    `gorm:"type:varchar(6);default:4CAF50" json:"color"`
 
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
